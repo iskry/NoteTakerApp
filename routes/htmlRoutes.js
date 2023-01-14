edd9d1e27ecef086db15ae6a9bba0c8b9121a2a4
iskry <refocuslucifer@protonmail.com>
2023-01-14 14:05:06 -0600

    added apiRoutes functionality and added expressRouter to fix indexjs/html bug.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,12 +1,16 @@
 const path = require('path'); // importing the path module
+const router = require('express').Router(); // requiring the express router 
 
-module.exports = (app) => { // exporting a function that takes in the express application instance
-
-app.get('/notes', (req, res) => { // handling GET request to the '/notes' route
-    res.sendFile(path.join(__dirname + '../public/notes.html')); // using the path.join method to join the current directory and the path to the notes.html file and sending the file using the res.sendFile method
+router.get('/notes', (req, res) => {  // handling GET request to the '/notes' route
+    res.sendFile(path.join(__dirname, '../public/notes.html'))  // using the path.join method to join the current directory and the path to the notes.html file and sending the file using the res.sendFile method
 });
 
-app.get('*', (req, res) => { // handling GET request to any route
-    res.sendFile(path.join(__dirname, '../public/index.html')); // using the path.join method to join the current directory and the path to the index.html file and sending the file using the res.sendFile method
-  })
-};
+router.get('/', (req, res) => { // handling GET request to the root route
+    res.sendFile(path.join(__dirname, '../public/index.html'));  // using the path.join method to join the current directory and the path to the index.html file and sending the file using the res.sendFile method
+});
+
+router.get('*', (req, res) => {  // handling GET request to any route
+    res.sendFile(path.join(__dirname, '../public/index.html'));  // using the path.join method to join the current directory and the path to the index.html file and sending the file using the res.sendFile method
+})
+
+module.exports = router; //exporting the router to be used in other parts of the application
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express'); // importing the express module
 const app = express(); // instantiating an express application
 const PORT = process.env.PORT || 3001; // setting the port for the server to run on, either from the environment variable or 3001
-require('./routes/htmlRoutes.js')(app); // requiring and executing the exported function from the htmlRoutes.js file, passing in the express application instance as an argument  
+const apiRoutes = require('./routes/apiRoutes'); // requiring the apiRoutes.js file
+const htmlRoutes = require('./routes/htmlRoutes'); // requiring the htmlRoutes.js file
 
 app.use(express.static('public')); // using the express.static middleware to serve files from the 'public' folder
+app.use('/api', apiRoutes)  // using the apiRoutes.js file for all routes starting with '/api'
+app.use('/', htmlRoutes) // using the htmlRoutes.js file for all routes starting with '/'
 app.use(express.urlencoded({ extended: true })); // using the express.urlencoded middleware to parse incoming request bodies
 app.use(express.json()); // using the express.json middleware to parse incoming JSON request bodies
 
